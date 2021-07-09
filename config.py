691535050e3a4de48df19b1329ce991d514aeb60
BobEsponjeYT <75587701+BobEsponjeYT@users.noreply.github.com>
2021-07-09 00:20:17 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
-
+class config:
+  
+  FILE="" ## FILE NAME + EXTENSION || example.txt || or other extension
+  
+class message:
+  
+  TEXT = """
+  Example1
+  Example 2
+  Example 3 _
+  Example Four 4 * 4
+  """ 

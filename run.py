cc8f8f59e8dc271ddd6643e076580386e3fd59be
BobEsponjeYT <75587701+BobEsponjeYT@users.noreply.github.com>
2021-07-09 00:45:26 -0300

    Run created :)

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-from config import FILE
+from config import config
+from config import message
 from time import sleep as wait
+import datetime
 from src import write
+import os
+
+def run():
+  write.__WRITE__(file=config.FILE, text=message.TEXT)
+  wait(5)
+  print("The file '{}' was written as {} by {} !".format(config.FILE, datetime.datetime.utcnow, os.name)")
+        
+run()

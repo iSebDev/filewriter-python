cb1cf72cb8fae312bbde0bde0f3501a234b63ac6
BobEsponjeYT <75587701+BobEsponjeYT@users.noreply.github.com>
2021-07-09 00:33:31 -0300

    Create write.py

---
+++ src/write.py
@@ -0,0 +1,23 @@
+import time
+
+import datetime
+
+from time import sleep as wait
+
+def __WRITE__(self, file=None, text=None):
+  
+  if not file:
+    
+    print("Please, set a file in config.py")
+   
+  elif(file == None):
+    
+    print("Please, set a file in config.py")
+    
+  else:
+    
+    with open(file, 'w') as f:
+      
+      f.write(text)
+      
+      

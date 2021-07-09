b94d2e43d013d3ec9ee6baf7444e3602717c22f1
BobEsponjeYT <75587701+BobEsponjeYT@users.noreply.github.com>
2021-07-09 19:44:53 -0300

    Run fixed!

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-from config import config
 from config import message
 from time import sleep as wait
 import datetime
@@ -8,6 +7,8 @@ import os
 def run():
   write.__WRITE__(file=config.FILE, text=message.TEXT)
   wait(5)
-  print("The file '{}' was written as {} by {} !".format(config.FILE, datetime.datetime.utcnow, os.name)")
+  print(f"File: {str(os.path.isfile(config.FILE))}")
+  print("The file '{}' was written as {} by {} !".format(config.FILE, datetime.datetime.utcnow(), os.name))
+  wait(10)
         
 run()

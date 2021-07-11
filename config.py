dc39d4f93e5338e93a2fded1a056029e675494ce
BobEsponjeYT <75587701+BobEsponjeYT@users.noreply.github.com>
2021-07-11 00:01:17 -0300

    New variables on Text

--- config.py
+++ config.py
@@ -4,9 +4,15 @@ class config:
   
 class message:
   
-  TEXT = """
+  TEXT = f"""
   Example1
   Example 2
   Example 3 _
   Example Four 4 * 4
+  {variables.ExampleText} + {variables.ExampleCalc} 
   """ 
+  
+class variables:
+  
+  ExampleText="Simple"
+  ExampleCalc=1+1

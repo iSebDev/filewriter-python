7498f666d7e39c525f9a26820fd60be90ecd8832
BobEsponjeYT <75587701+BobEsponjeYT@users.noreply.github.com>
2021-07-11 20:28:14 -0300

    Update variables.py

--- src/variables.py
+++ src/variables.py
@@ -1,1 +1,10 @@
+import datetime
+import random
 
+class vars:
+    variable1 = 1+1 # Simple sum in variable
+    variable2 = "Hi" # Simple str var
+    variable3 = "Hi {}".format(15-5) # Variables in str of var
+    variable4 = datetime.datetime.utcnow() # Get time
+    variable5 = random.randint(1,100) # Get random number variable
+    variable6 = random.choice([variable1,variable2,variable3]) # Get random variable

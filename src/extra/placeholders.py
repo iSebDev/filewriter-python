acff1aed3c8206074b848f38f37b42c7d774431b
BobEsponjeYT <75587701+BobEsponjeYT@users.noreply.github.com>
2021-07-12 12:44:28 -0300

    Prototype of placeholders, for variables
    This is in test

--- src/extra/placeholders.py
+++ src/extra/placeholders.py
@@ -1,1 +1,37 @@
+import time as times
 
+class placehold:
+  
+  def variable(name=None, var=None, time=None):
+    
+    if not name:
+      
+      try:
+        
+        except KeyError
+        
+    else:
+      
+      return name + var
+      return times.sleep(time)
+    
+    
+    if var == times.sleep:
+      
+      print("Error! Times not is a variable in placeholders")
+      
+      break
+      
+    for name in variable:
+      
+      if name[0] > name[20]:
+        
+        print("Error! Placeholder name error 'args > 20'.")
+        break
+        
+        
+      elif(name[0] > name[20]):
+        
+        return variable
+        
+       

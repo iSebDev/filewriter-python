524016c128fb0c5ae0e3813acb80e29ad40485b8
BobEsponjeYT <75587701+BobEsponjeYT@users.noreply.github.com>
2021-07-11 21:06:22 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,20 @@
+from config import config as conf
 from config import message
+from config import options
 from time import sleep as wait
 import datetime
 from src import write
 import os
+import json
 
 def run():
-  write.__WRITE__(file=config.FILE, text=message.TEXT)
+  write.__WRITE__(file=conf.FILE, text=message.TEXT)
   wait(5)
-  print(f"File: {str(os.path.isfile(config.FILE))}")
-  print("The file '{}' was written as {} by {} !".format(config.FILE, datetime.datetime.utcnow(), os.name))
+  print(f"File: {str(os.path.isfile(conf.FILE))}")
+  if options.os_aliases == True:
+    print("The file '{}' was written as {} by {} !".format(conf.FILE, datetime.datetime.utcnow(), conf.OS_ALIAS))
+  else:
+    print("The file '{}' was written as {} by {} !".format(conf.FILE, datetime.datetime.utcnow(), os.name))
   wait(10)
         
 run()
